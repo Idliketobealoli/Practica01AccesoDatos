8d9a17db76c77305d9392865b688025c06a80016
Daniel Rodríguez Muñoz <daniel.rodriguez@alumno.iesluisvives.org>
2021-10-20 10:30:50 +0200

    Minor tweaks.

--- src/main/java/LectorCSV.java
+++ src/main/java/LectorCSV.java
@@ -1,4 +1,6 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,35 +10,50 @@ import java.nio.file.StandardCopyOption;
 public class LectorCSV {
     Path thisPath = Paths.get(".");
     Path resources = Paths.get(thisPath + File.separator + "src" + File.separator + "main" + File.separator + "resources");
-    Path results = Paths.get(resources + File.separator + "results");
+
+    // lee el csv linea a linea y lo devuelve como String
+    public String readCSV(String nameOfFile) {
+        Path csv = Paths.get(resources + File.separator + nameOfFile);
+        StringBuilder sb = new StringBuilder();
+        if (Files.exists(csv) && !Files.isDirectory(csv) && Files.isReadable(csv)) {
+            try {
+                Files.readAllLines(csv).forEach(x -> sb.append(x + "\n"));
+            } catch (IOException e) {
+                System.err.println(e);
+            }
+        }
+        else {
+            System.out.println("'" + nameOfFile + "' does not exist, is a directory or is not readable.");
+        }
+        return sb.toString();
+    }
 
     //este método leerá el csv línea a línea, filtrando los campos que necesitemos más adelante en función de qué csv sea.
-    public String buildNewCSVcontent(String nameOfFile) {
-        StringBuilder sBuilder = new StringBuilder();
+    public String buildNewCSVcontent(String nameOfFile, String city) {
+        String result = "";
         Path csv = Paths.get(resources + File.separator + nameOfFile);
-        // ProcessorCSV pcsv = new ProcessorCSV();
-        try {
-            if (Files.exists(csv) && !Files.isDirectory(csv) && Files.isReadable(csv)) {
-                Files.readAllLines(csv).stream().forEach(x -> sBuilder.append(/* pcsv.processCSV(nameOfFile, */x/*)*/));
-            }
-            else {
-                System.out.println("'" + nameOfFile + "' does not exist, is a directory or is not readable.");
-            }
-        } catch (IOException e) {
-            System.err.println(e);
+        ProcessorCSV pcsv = new ProcessorCSV();
+        if (Files.exists(csv) && !Files.isDirectory(csv) && Files.isReadable(csv)) {
+            // Files.readAllLines(csv).stream().forEach(x -> sBuilder.append(pcsv.processCSV(nameOfFile, x)));
+            result = pcsv.processCSV(csv, city);
         }
-        return sBuilder.toString();
+        else {
+            System.out.println("'" + nameOfFile + "' does not exist, is a directory or is not readable.");
+        }
+        return result;
     }
 
     // Copia un csv cuyo nombre le pases por parámetro de la carpeta resources a la carpeta copies.
     public void saveModifiedCSV(String nameOfFile, String content) {
         try {
-            // esto guarda en la variable "csv" el path del archivo de la carpeta resources que queramos leer.
-            Path csv = Paths.get(resources + File.separator + nameOfFile);
-            Path csvCopia = Paths.get( results + File.separator + nameOfFile);
-            // Crea una copia del archivo que haya en el path del primer parámetro en el path del segundo parámetro.
-            // Si el archivo ya existe, lo reemplaza.
-            Files.copy(csv, csvCopia, StandardCopyOption.REPLACE_EXISTING);
+            Path csvModified = Paths.get( resources + File.separator + "copia_" + nameOfFile);
+            csvModified.toFile().createNewFile();
+            File file = new File(csvModified.toString());
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(content);
+            bw.close();
+            fw.close();
         } catch (IOException e) {
             System.err.println(e);
         }
