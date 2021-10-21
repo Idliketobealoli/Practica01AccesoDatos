803fb29e33655c451e58907e26a578189b651f60
Daniel Rodríguez Muñoz <daniel.rodriguez@alumno.iesluisvives.org>
2021-10-21 14:28:48 +0200

    Minor refactoring. - New changes: now the POJO classes Calidad_aire_datos_mes.java and Calidad_aire_datos_meteo_mes.java feature an attribute listH and listV which consist of a list of integers and characters respectively. This way they have way less attributes and said attributes are easier to access and link each H with their respective V.

--- src/main/java/model/Calidad_aire_datos_meteo_mes.java
+++ src/main/java/model/Calidad_aire_datos_meteo_mes.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,52 +19,6 @@ public class Calidad_aire_datos_meteo_mes {
     private int ano;
     private int mes;
     private int dia;
-    private int h1;
-    private char v1;
-    private int h2;
-    private char v2;
-    private int h3;
-    private char v3;
-    private int h4;
-    private char v4;
-    private int h5;
-    private char v5;
-    private int h6;
-    private char v6;
-    private int h7;
-    private char v7;
-    private int h8;
-    private char v8;
-    private int h9;
-    private char v9;
-    private int h10;
-    private char v10;
-    private int h11;
-    private char v11;
-    private int h12;
-    private char v12;
-    private int h13;
-    private char v13;
-    private int h14;
-    private char v14;
-    private int h15;
-    private char v15;
-    private int h16;
-    private char v16;
-    private int h17;
-    private char v17;
-    private int h18;
-    private char v18;
-    private int h19;
-    private char v19;
-    private int h20;
-    private char v20;
-    private int h21;
-    private char v21;
-    private int h22;
-    private char v22;
-    private int h23;
-    private char v23;
-    private int h24;
-    private char v24;
+    private List<Integer> listH = new ArrayList<>(24);
+    private List<Character> listV = new ArrayList<>(24);
 }
--- src/main/java/utils/Util.java
+++ src/main/java/utils/Util.java
@@ -38,54 +38,12 @@ public class Util {
             cadm.setAno(Integer.parseInt(st.nextToken()));
             cadm.setMes(Integer.parseInt(st.nextToken()));
             cadm.setDia(Integer.parseInt(st.nextToken()));
-            cadm.setH1(Integer.parseInt(st.nextToken()));
-            cadm.setV1(st.nextToken().charAt(0));
-            cadm.setH2(Integer.parseInt(st.nextToken()));
-            cadm.setV2(st.nextToken().charAt(0));
-            cadm.setH3(Integer.parseInt(st.nextToken()));
-            cadm.setV3(st.nextToken().charAt(0));
-            cadm.setH4(Integer.parseInt(st.nextToken()));
-            cadm.setV4(st.nextToken().charAt(0));
-            cadm.setH5(Integer.parseInt(st.nextToken()));
-            cadm.setV5(st.nextToken().charAt(0));
-            cadm.setH6(Integer.parseInt(st.nextToken()));
-            cadm.setV6(st.nextToken().charAt(0));
-            cadm.setH7(Integer.parseInt(st.nextToken()));
-            cadm.setV7(st.nextToken().charAt(0));
-            cadm.setH8(Integer.parseInt(st.nextToken()));
-            cadm.setV8(st.nextToken().charAt(0));
-            cadm.setH9(Integer.parseInt(st.nextToken()));
-            cadm.setV9(st.nextToken().charAt(0));
-            cadm.setH10(Integer.parseInt(st.nextToken()));
-            cadm.setV10(st.nextToken().charAt(0));
-            cadm.setH11(Integer.parseInt(st.nextToken()));
-            cadm.setV11(st.nextToken().charAt(0));
-            cadm.setH12(Integer.parseInt(st.nextToken()));
-            cadm.setV12(st.nextToken().charAt(0));
-            cadm.setH13(Integer.parseInt(st.nextToken()));
-            cadm.setV13(st.nextToken().charAt(0));
-            cadm.setH14(Integer.parseInt(st.nextToken()));
-            cadm.setV14(st.nextToken().charAt(0));
-            cadm.setH15(Integer.parseInt(st.nextToken()));
-            cadm.setV15(st.nextToken().charAt(0));
-            cadm.setH16(Integer.parseInt(st.nextToken()));
-            cadm.setV16(st.nextToken().charAt(0));
-            cadm.setH17(Integer.parseInt(st.nextToken()));
-            cadm.setV17(st.nextToken().charAt(0));
-            cadm.setH18(Integer.parseInt(st.nextToken()));
-            cadm.setV18(st.nextToken().charAt(0));
-            cadm.setH19(Integer.parseInt(st.nextToken()));
-            cadm.setV19(st.nextToken().charAt(0));
-            cadm.setH20(Integer.parseInt(st.nextToken()));
-            cadm.setV20(st.nextToken().charAt(0));
-            cadm.setH21(Integer.parseInt(st.nextToken()));
-            cadm.setV21(st.nextToken().charAt(0));
-            cadm.setH22(Integer.parseInt(st.nextToken()));
-            cadm.setV22(st.nextToken().charAt(0));
-            cadm.setH23(Integer.parseInt(st.nextToken()));
-            cadm.setV23(st.nextToken().charAt(0));
-            cadm.setH24(Integer.parseInt(st.nextToken()));
-            cadm.setV24(st.nextToken().charAt(0));
+            while (st.hasMoreTokens()) {
+                // ojo, si queda exactamente un token, se meterá en el bucle y añadirá uno a
+                // listH, pero explotará cuando intente meter uno a listV, pues no quedarían más.
+                cadm.getListH().add(Integer.parseInt(st.nextToken()));
+                cadm.getListV().add(st.nextToken().charAt(0));
+            }
             // una vez seteado todo, lo anadimos a la lista de objetos cadm
             cadmList.add(cadm);
         }
@@ -109,54 +67,10 @@ public class Util {
             cadmm.setAno(Integer.parseInt(st.nextToken()));
             cadmm.setMes(Integer.parseInt(st.nextToken()));
             cadmm.setDia(Integer.parseInt(st.nextToken()));
-            cadmm.setH1(Integer.parseInt(st.nextToken()));
-            cadmm.setV1(st.nextToken().charAt(0));
-            cadmm.setH2(Integer.parseInt(st.nextToken()));
-            cadmm.setV2(st.nextToken().charAt(0));
-            cadmm.setH3(Integer.parseInt(st.nextToken()));
-            cadmm.setV3(st.nextToken().charAt(0));
-            cadmm.setH4(Integer.parseInt(st.nextToken()));
-            cadmm.setV4(st.nextToken().charAt(0));
-            cadmm.setH5(Integer.parseInt(st.nextToken()));
-            cadmm.setV5(st.nextToken().charAt(0));
-            cadmm.setH6(Integer.parseInt(st.nextToken()));
-            cadmm.setV6(st.nextToken().charAt(0));
-            cadmm.setH7(Integer.parseInt(st.nextToken()));
-            cadmm.setV7(st.nextToken().charAt(0));
-            cadmm.setH8(Integer.parseInt(st.nextToken()));
-            cadmm.setV8(st.nextToken().charAt(0));
-            cadmm.setH9(Integer.parseInt(st.nextToken()));
-            cadmm.setV9(st.nextToken().charAt(0));
-            cadmm.setH10(Integer.parseInt(st.nextToken()));
-            cadmm.setV10(st.nextToken().charAt(0));
-            cadmm.setH11(Integer.parseInt(st.nextToken()));
-            cadmm.setV11(st.nextToken().charAt(0));
-            cadmm.setH12(Integer.parseInt(st.nextToken()));
-            cadmm.setV12(st.nextToken().charAt(0));
-            cadmm.setH13(Integer.parseInt(st.nextToken()));
-            cadmm.setV13(st.nextToken().charAt(0));
-            cadmm.setH14(Integer.parseInt(st.nextToken()));
-            cadmm.setV14(st.nextToken().charAt(0));
-            cadmm.setH15(Integer.parseInt(st.nextToken()));
-            cadmm.setV15(st.nextToken().charAt(0));
-            cadmm.setH16(Integer.parseInt(st.nextToken()));
-            cadmm.setV16(st.nextToken().charAt(0));
-            cadmm.setH17(Integer.parseInt(st.nextToken()));
-            cadmm.setV17(st.nextToken().charAt(0));
-            cadmm.setH18(Integer.parseInt(st.nextToken()));
-            cadmm.setV18(st.nextToken().charAt(0));
-            cadmm.setH19(Integer.parseInt(st.nextToken()));
-            cadmm.setV19(st.nextToken().charAt(0));
-            cadmm.setH20(Integer.parseInt(st.nextToken()));
-            cadmm.setV20(st.nextToken().charAt(0));
-            cadmm.setH21(Integer.parseInt(st.nextToken()));
-            cadmm.setV21(st.nextToken().charAt(0));
-            cadmm.setH22(Integer.parseInt(st.nextToken()));
-            cadmm.setV22(st.nextToken().charAt(0));
-            cadmm.setH23(Integer.parseInt(st.nextToken()));
-            cadmm.setV23(st.nextToken().charAt(0));
-            cadmm.setH24(Integer.parseInt(st.nextToken()));
-            cadmm.setV24(st.nextToken().charAt(0));
+            while (st.hasMoreTokens()) {
+                cadmm.getListH().add(Integer.parseInt(st.nextToken()));
+                cadmm.getListV().add(st.nextToken().charAt(0));
+            }
             cadmmList.add(cadmm);
         }
         return cadmmList;
@@ -204,7 +118,7 @@ public class Util {
         Path csv = Paths.get(System.getProperty("user.dir") + File.separator + "src" + File.separator +
                 "main" + File.separator + "resources" + File.separator + "calidad_aire_zonas.csv");
         final List<String> lines = Files.readAllLines(csv);
-        List<Calidad_aire_zonas> cazList = new ArrayList<Calidad_aire_zonas>();
+        List<Calidad_aire_zonas> cazList = new ArrayList<>();
         for (int i = 1; i < lines.size(); i++) {
             StringTokenizer st = new StringTokenizer(lines.get(i), ";");
             Calidad_aire_zonas caz = new Calidad_aire_zonas();
